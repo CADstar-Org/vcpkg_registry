2cc33ea63f72d7a842667d4f3b84cf191dacbff4
zell <m.naghizadeh91@gmail.com>
2022-10-20 10:31:00 +0200

    export .vcpkg-root to output

--- export.ps1
+++ export.ps1
@@ -10,6 +10,7 @@ if(Test-Path -Path "./vcpkg-export") {
 New-Item './vcpkg-export/installed' -ItemType Directory
 
 Copy-Item './vcpkg_installed\*' -Destination './vcpkg-export/installed' -Recurse
-Copy-Item './vcpkg/scripts'-Destination './vcpkg-export' -Recurse
+Copy-Item './vcpkg/scripts' -Destination './vcpkg-export' -Recurse
+Copy-Item './vcpkg/.vcpkg-root' -Destination './vcpkg-export'
 
 7z.exe a export-$date.7z .\vcpkg-export
