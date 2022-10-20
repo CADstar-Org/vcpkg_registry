8298fd0541a55b9e7a8708b6a1c3425b308bb67b
zell <m.naghizadeh91@gmail.com>
2022-10-20 15:53:16 +0200

    more fix from opengv add new library

--- ports/opengv/portfile.cmake
+++ ports/opengv/portfile.cmake
@@ -11,42 +11,9 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share/libvh)
+
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 # Handle copyright
 file(INSTALL ${SOURCE_PATH}/License.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/opengv RENAME copyright)
-
-# vcpkg_extract_source_archive_ex(
-    # OUT_SOURCE_PATH SOURCE_PATH
-    # NO_REMOVE_ONE_LEVEL
-    # ARCHIVE ${ARCHIVE}
-    # REF opengv
-# )
-
-#file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION ${SOURCE_PATH})
-
-# vcpkg_configure_cmake(
-#     SOURCE_PATH ${SOURCE_PATH}
-# )
-
-# vcpkg_install_cmake()
-
-# file(WRITE ${CURRENT_PACKAGES_DIR}/share/wibu/copyright.txt "LICENSE CODE GOES HERE")
-
-# # Install CMake package definitions
-# file(WRITE "${CURRENT_PACKAGES_DIR}/share/opengv/opengvConfig.cmake")
-# # This script sets following variables:
-# # OPENGV_FOUND
-# # OPENGV_INCLUDE_DIR
-# # OPENGV_LIBRARIES_DIR
-# # OPENGV_LIBRARIES
-# set(opengv_FOUND TRUE)
-# set(opengv_INCLUDE_DIR \${CMAKE_INSTALL_PREFIX}/include)
-# set(opengv_LIBRARIES_DIR \${CMAKE_INSTALL_PREFIX}/lib)
-# # Search for libraries
-# find_library(opengv_LIBRARIES NAMES \"opengv\" \"opengv.lib\")
-# # Moves all .cmake files from /debug/share/cupoch/ to /share/cupoch/
-# # See /docs/maintainers/vcpkg_fixup_cmake_targets.md for more details
-# #vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share/opengv)
-# # Handle copyright
-# #file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/opengv RENAME copyright)
