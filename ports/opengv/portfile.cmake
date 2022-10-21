ec4d4ebf8a6a2d47d32211b8b27325cc84c9c318
zell <m.naghizadeh91@gmail.com>
2022-10-21 13:19:41 +0200

    change cgal to 5.3 update opengv share folder

--- ports/opengv/portfile.cmake
+++ ports/opengv/portfile.cmake
@@ -11,7 +11,7 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake TARGET_PATH share/libvh)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT} TARGET_PATH share/opengv)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
